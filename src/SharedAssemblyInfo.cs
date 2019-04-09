beaab9213eb66c5e5822b1dc85fca40983719a10
Adam Barclay <barclay.adam@gmail.com>
2019-04-09 09:58:59 +0100

    Bump to v4.0.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 
 // Don't edit manually! Use `build.bat version` command instead!
-[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyVersion("4.0.0")]
