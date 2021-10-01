8d8746c1efef34282687f8eb451b984f090b351c
Evolutionlab <info@evolutionlab.it>
2021-10-02 00:33:08 +0200

    globalconfiguration extensions

---
+++ src/HangFire.Azure.ServiceBusQueue/ServiceBusGlobalConfigurationExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Azure.Messaging.ServiceBus.Administration;
+using Hangfire.Annotations;
+using Hangfire.SqlServer;
+using Hangfire.States;
+
+namespace Hangfire.Azure.ServiceBusQueue
+{
+    public static class ServiceBusGlobalConfigurationExtensions
+    {
+        public static IGlobalConfiguration UseServiceBusQueues(
+            [NotNull] this IGlobalConfiguration<SqlServerStorage> configuration,
+            [NotNull] string connectionString)
+        {
+            return UseServiceBusQueues(configuration, new ServiceBusQueueOptions
+            {
+                ConnectionString = connectionString,
+                Queues           = new[] { EnqueuedState.DefaultQueue }
+            });
+        }
+
+        public static IGlobalConfiguration UseServiceBusQueues(
+            [NotNull] this IGlobalConfiguration<SqlServerStorage> configuration,
+            [NotNull] string connectionString,
+            params string[] queues)
+        {
+            return UseServiceBusQueues(configuration, new ServiceBusQueueOptions
+            {
+                ConnectionString = connectionString,
+                Queues           = queues
+            });
+        }
+
+        public static IGlobalConfiguration UseServiceBusQueues(
+            [NotNull] this IGlobalConfiguration<SqlServerStorage> configuration,
+            [NotNull] string connectionString,
+            Action<CreateQueueOptions> configureAction,
+            params string[] queues)
+        {
+            return UseServiceBusQueues(configuration, new ServiceBusQueueOptions
+            {
+                ConnectionString = connectionString,
+                Configure        = configureAction,
+                Queues           = queues
+            });
+        }
+
+        public static IGlobalConfiguration UseServiceBusQueues(
+            [NotNull] this IGlobalConfiguration<SqlServerStorage> configuration,
+            [NotNull] ServiceBusQueueOptions options)
+        {
+            var sqlServerStorage = configuration.Entry;
+            var provider         = new ServiceBusQueueJobQueueProvider(options);
+            sqlServerStorage.QueueProviders.Add(provider, options.Queues);
+            return configuration;
+        }
+    }
+}
