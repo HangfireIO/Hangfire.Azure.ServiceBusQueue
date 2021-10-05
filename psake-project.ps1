2788ff571456a9aec9bbf99562d31b4b9d6e6e9c
Sergey Odinokov <odinserj@gmail.com>
2021-10-05 19:25:29 +0100

    Enable Hangfire.Build logic for CI

--- psake-project.ps1
+++ psake-project.ps1
@@ -2,22 +2,21 @@ Properties {
     $solution = "HangFire.Azure.ServiceBusQueue.sln"
 }
 
-Include "packages\Hangfire.Build.0.1.3\tools\psake-common.ps1"
+Include "packages\Hangfire.Build.0.2.6\tools\psake-common.ps1"
 
 Task Default -Depends Collect
 
-Task Collect -Depends Compile -Description "Copy all artifacts to the build folder." {
-    Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "Net45"
+Task CompileCore -Depends Clean {
+    Exec { dotnet build -c Release }
+}
+
+Task Collect -Depends CompileCore -Description "Copy all artifacts to the build folder." {
+    Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "net461"
+    Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "netstandard2.0"
 }
 
 Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
-    $version = Get-BuildVersion
-
-    $tag = $env:APPVEYOR_REPO_TAG_NAME
-    if ($tag -And $tag.StartsWith("v$version-")) {
-        "Using tag-based version for packages."
-        $version = $tag.Substring(1)
-    }
+    $version = Get-PackageVersion
     
     Create-Archive "Hangfire.Azure.ServiceBusQueue-$version"
     Create-Package "Hangfire.Azure.ServiceBusQueue" $version
--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 
 // Don't edit manually! Use `build.bat version` command instead!
-[assembly: AssemblyVersion("4.1.0")]
+[assembly: AssemblyVersion("5.0.0")]
