b1e89a98afd3be3b8087fc29513b58acfae40ebc
Sergey Odinokov <odinserj@hangfire.io>
2025-04-14 06:18:54 +0700

    Upgrade Hangfire.Build to 0.5.0

--- psake-project.ps1
+++ psake-project.ps1
@@ -1,16 +1,8 @@
-Properties {
-    $solution = "HangFire.Azure.ServiceBusQueue.sln"
-}
-
-Include "packages\Hangfire.Build.0.2.6\tools\psake-common.ps1"
+Include "packages\Hangfire.Build.0.5.0\tools\psake-common.ps1"
 
 Task Default -Depends Collect
 
-Task CompileCore -Depends Clean {
-    Exec { dotnet build -c Release }
-}
-
-Task Collect -Depends CompileCore -Description "Copy all artifacts to the build folder." {
+Task Collect -Depends Compile -Description "Copy all artifacts to the build folder." {
     Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "net461"
     Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "netstandard2.0"
 }
@@ -18,6 +10,6 @@ Task Collect -Depends CompileCore -Description "Copy all artifacts to the build 
 Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
     $version = Get-PackageVersion
     
-    Create-Archive "Hangfire.Azure.ServiceBusQueue-$version"
     Create-Package "Hangfire.Azure.ServiceBusQueue" $version
+    Create-Archive "Hangfire.Azure.ServiceBusQueue-$version"
 }
