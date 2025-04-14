93f3a61e250e8f8f4728115d35f68a988a8df626
Sergey Odinokov <odinserj@hangfire.io>
2025-04-14 08:41:20 +0700

    Update NuSpec file

--- psake-project.ps1
+++ psake-project.ps1
@@ -5,6 +5,9 @@ Task Default -Depends Collect
 Task Collect -Depends Compile -Description "Copy all artifacts to the build folder." {
     Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "net461"
     Collect-Assembly "Hangfire.Azure.ServiceBusQueue" "netstandard2.0"
+    Collect-File "license.txt"
+    Collect-File "readme.md"
+    Collect-File "icon.png"
 }
 
 Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
